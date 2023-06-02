8b3efc47fe1f83ce30b8cbb39823bf033e8902dc
N_ha <69199126+N-ha-git@users.noreply.github.com>
2023-06-02 21:54:45 +0900

    Add social accounts list

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,16 @@
 import { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <div>Hello!</div>
+  return (<div>
+    <h1>N_ha</h1>
+    <p>Hello, World!</p>
+    <ul>
+      {/* Todo: コンポーネント化 */}
+      <li><a href="https://github.com/N-ha-1050">GitHub</a></li>
+      <li><a href="https://twitter.com/N_ha_ycu">Twitter</a></li>
+      <li><a href="https://atcoder.jp/users/N_ha_1050">AtCoder</a></li>
+    </ul>
+  </div>)
 }
 
 export default Home
