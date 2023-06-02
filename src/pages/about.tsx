fde5724e4291de88849b09476468f0ada9d1291b
N_ha <69199126+N-ha-git@users.noreply.github.com>
2023-06-02 21:02:35 +0900

    init

---
+++ src/pages/about.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from "next";
+
+const About: NextPage = () => {
+    return (
+    <div>
+        <h1>About</h1>
+        <p>プロフィール</p>
+    </div>
+    )
+}
+
+export default About
