d25d110140aab58cb2e2b2c12a522dfaf4fc1075
N_ha <69199126+N-ha-git@users.noreply.github.com>
2023-06-05 16:32:34 +0900

    create SocialLink component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,28 @@
 import { NextPage } from "next";
+import { ReactElement } from "react";
+
+type SocialLinkProps = {
+  url: string
+  display_name: string
+}
+const SocialLink = ({url, display_name}: SocialLinkProps) => {
+  return (
+    <li><a href={url}>{display_name}</a></li>
+  )
+}
 
 const Home: NextPage = () => {
-  return (<div>
-    <h1>N_ha</h1>
-    <p>Hello, World!</p>
-    <ul>
-      {/* Todo: コンポーネント化 */}
-      <li><a href="https://github.com/N-ha-1050">GitHub</a></li>
-      <li><a href="https://twitter.com/N_ha_ycu">Twitter</a></li>
-      <li><a href="https://atcoder.jp/users/N_ha_1050">AtCoder</a></li>
-    </ul>
-  </div>)
+  return (
+    <div>
+      <h1>N_ha</h1>
+      <p>Hello, World!</p>
+      <ul>
+        <SocialLink url='https://github.com/N-ha-1050' display_name='GitHub' />
+        <SocialLink url='https://twitter.com/N_ha_ycu' display_name='Twitter' />
+        <SocialLink url='https://atcoder.jp/users/N_ha_1050' display_name='AtCoder' />
+      </ul>
+    </div>
+  )
 }
 
 export default Home
