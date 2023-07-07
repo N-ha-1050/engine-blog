e07a05e790bbc4229f1b0fa4ff205d51d5e2f709
N_ha <69199126+N-ha-git@users.noreply.github.com>
2023-07-08 06:16:23 +0900

    ブログタイトルの変更

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,10 +5,16 @@ const Header = () => {
     <header className="flex items-center py-2.5 px-5 max-w-7xl mx-auto">
       <Link
         href="/"
+        className="text-blue-950 border-b duration-200 border-transparent hover:border-blue-950"
+      >
+        活動記録
+      </Link>
+      {/* <Link
+        href="/"
         className="text-4xl py-5 px-5 bg-blue-400 text-blue-950 rounded-full"
       >
         N_ha
-      </Link>
+      </Link> */}
       <div className="flex-1" />
       <button className="text-blue-950">Menu</button>
     </header>
