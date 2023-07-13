6b8aabf4329f511b731f930280f71afa0e63d15c
N_ha <69199126+N-ha-git@users.noreply.github.com>
2023-07-13 23:04:57 +0900

    gitignore 修正

---
+++ src/pages/posts.tsx
@@ -0,0 +1,40 @@
+import { Post } from '@/types/post'
+import { GetStaticProps, NextPage } from 'next'
+import Link from 'next/link'
+import fs from 'fs/promises'
+import { PostPreview } from '@/components/PostPreview'
+
+type Props = {
+  posts: Post[]
+}
+
+const PostList: NextPage<Props> = ({ posts }) => {
+  return (
+    <div>
+      <h1 className="mb-8 text-4xl">一覧</h1>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-3">
+        {posts.map((post) => (
+          <PostPreview key={post.id} post={post} />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  const postsDir = `${process.cwd()}/src/pages/posts`
+  const filenames = await fs.readdir(postsDir)
+  const posts: Post[] = await Promise.all(
+    filenames.map(async (filename) => {
+      const { metadata } = await import(`./posts/${filename}`)
+      return metadata
+    })
+  )
+  return {
+    props: {
+      posts,
+    },
+  }
+}
+
+export default PostList
