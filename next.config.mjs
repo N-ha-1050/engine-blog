c063441d6dfc9ffb292bc649411cd029dcfa057e
N_ha <69199126+N-ha-git@users.noreply.github.com>
2023-08-02 09:10:01 +0900

    reamrk と rehype のプラグイン追加

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,20 @@
-// next.config.js
+// next.config.mjs
 
-const withMDX = require('@next/mdx')({
+import createMDX from '@next/mdx'
+import rehypeHighlight from 'rehype-highlight'
+import rehypeKatex from 'rehype-katex'
+import remarkFrontmatter from 'remark-frontmatter'
+import remarkGfm from 'remark-gfm'
+import remarkMath from 'remark-math'
+
+const withMDX = createMDX({
   extension: /\.mdx?$/,
   options: {
     // If you use remark-gfm, you'll need to use next.config.mjs
     // as the package is ESM only
     // https://github.com/remarkjs/remark-gfm#install
-    remarkPlugins: [],
-    rehypePlugins: [],
+    remarkPlugins: [remarkGfm, remarkFrontmatter, remarkMath],
+    rehypePlugins: [rehypeKatex, rehypeHighlight],
     // If you use `MDXProvider`, uncomment the following line.
     // providerImportSource: "@mdx-js/react",
   },
@@ -22,4 +29,5 @@ const nextConfig = {
 }
 
 // Merge MDX config with Next.js config
-module.exports = withMDX(nextConfig)
+// module.exports = withMDX(nextConfig)
+export default withMDX(nextConfig)
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,24 @@ export default function Document() {
     <Html lang="ja">
       <Head>
         <link rel="icon" href="/favicon.svg" />
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/katex@0.16.0/dist/katex.min.css"
+          integrity="sha384-Xi8rHCmBmhbuyyhbI88391ZKP2dmfnOl4rT9ZfRI7mLTdk1wblIUnrIq35nqwEvC"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/styles/mono-blue.min.css"
+        />
+        {/* <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/styles/base16/google-dark.min.css"
+        /> */}
+        {/* <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/styles/github-dark.min.css"
+        /> */}
       </Head>
       <body className="font-sansJp">
         <Main />
