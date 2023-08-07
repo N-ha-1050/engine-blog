8f81f6fc2f12c1188e470f51ea83ac3dc0aecc31
N-ha-1050 <69199126+N-ha-1050@users.noreply.github.com>
2023-08-07 18:05:14 +0900

    Update next.config.mjs
    Co-authored-by: NakamuraTakumi <104000239+nyatinte@users.noreply.github.com>

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,6 @@
 import createMDX from '@next/mdx'
 import rehypeHighlight from 'rehype-highlight'
 import rehypeKatex from 'rehype-katex'
-// import remarkFrontmatter from 'remark-frontmatter'
 import remarkGfm from 'remark-gfm'
 import remarkMath from 'remark-math'
 
